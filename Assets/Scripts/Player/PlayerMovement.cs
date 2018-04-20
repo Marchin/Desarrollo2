0a7ef6648e83bd4c0bac776fe3f1e6f7bfe27fef
Marchin <mmonterrosa94@gmail.com>
2018-04-19 22:42:59 -0300

    Initial commit

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	[SerializeField] float movementSpeed;
+	[Range(0f,90f)]
+	[SerializeField] float rotarionSpeed;
+	Rigidbody playerRigidBody;
+
+	void Start() {
+		playerRigidBody = GetComponent<Rigidbody>();
+	}
+
+	void Update() {
+		float moveVertical = Input.GetAxis("Vertical");
+		float moveHorizontal = Input.GetAxis("Horizontal");
+
+		Vector3 movement = new Vector3(0f, 0.0f, moveVertical);
+		playerRigidBody.velocity = movement * movementSpeed * Time.deltaTime;
+		Vector3 rotation = new Vector3(0f,	moveHorizontal, 0f);
+		playerRigidBody.rotation = Quaternion.Euler ( rotation * rotarionSpeed * Time.deltaTime);
+	}
+}
