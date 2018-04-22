b37f0b1922352284a6ac0ad5d1b1d8ef6b1c0072
Marchin <mmonterrosa94@gmail.com>
2018-04-21 22:50:46 -0300

    Movement and Visuals

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 	[SerializeField] float movementSpeed;
-	[Range(0f,90f)]
-	[SerializeField] float rotarionSpeed;
+	// [Range(0f,90f)]
+	[SerializeField] float rotationSpeed;
 	Rigidbody playerRigidBody;
 
 	void Start() {
 		playerRigidBody = GetComponent<Rigidbody>();
 	}
 
-	void Update() {
+	void FixedUpdate() {
 		float moveVertical = Input.GetAxis("Vertical");
 		float moveHorizontal = Input.GetAxis("Horizontal");
 
 		Vector3 movement = new Vector3(0f, 0.0f, moveVertical);
-		playerRigidBody.velocity = movement * movementSpeed * Time.deltaTime;
-		Vector3 rotation = new Vector3(0f,	moveHorizontal, 0f);
-		playerRigidBody.rotation = Quaternion.Euler ( rotation * rotarionSpeed * Time.deltaTime);
+		// playerRigidBody.velocity = movement * movementSpeed * Time.deltaTime;
+		playerRigidBody.AddRelativeForce(movement * movementSpeed);
+		Vector3 rotation = new Vector3(0f, moveHorizontal, 0f)* rotationSpeed;
+		// playerRigidBody.MoveRotation(Quaternion.Euler((transform.rotation.eulerAngles)* Time.deltaTime)* playerRigidBody.rotation);
+		playerRigidBody.AddRelativeTorque(rotation);
 	}
 }
