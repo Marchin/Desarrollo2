56b0b10ea5576ad4824610fc74ad82e4b5a70dd9
Marchin <mmonterrosa94@gmail.com>
2018-04-25 10:46:10 -0300

    Rocks

---
+++ Assets/Scripts/Player/CameraMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+	[SerializeField] float rotationSpeed;
+	[SerializeField] float min;
+	[SerializeField] float max;
+	Rigidbody playerRigidBody;
+
+	void Start() {
+		playerRigidBody = GetComponent<Rigidbody>();
+	}
+
+	void Update() {
+		transform.rotation = Quaternion.Euler(new Vector3(
+			Mathf.Clamp(transform.rotation.x, min, max),
+			transform.rotation.y,
+			transform.rotation.z)
+		);
+	}
+
+	void FixedUpdate() {
+		float cameraHorizontal = Input.GetAxis("Mouse X");
+		float cameraVertical = Input.GetAxis("Mouse Y");
+		Vector3 rotation = new Vector3(cameraHorizontal, cameraVertical, 0f)* rotationSpeed;
+		playerRigidBody.AddRelativeTorque(rotation);
+	}
+
+}
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 	[SerializeField] float movementSpeed;
-	// [Range(0f,90f)]
-	[SerializeField] float rotationSpeed;
 	Rigidbody playerRigidBody;
 
 	void Start() {
@@ -16,11 +14,7 @@ public class PlayerMovement : MonoBehaviour {
 		float moveVertical = Input.GetAxis("Vertical");
 		float moveHorizontal = Input.GetAxis("Horizontal");
 
-		Vector3 movement = new Vector3(0f, 0.0f, moveVertical);
-		// playerRigidBody.velocity = movement * movementSpeed * Time.deltaTime;
+		Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
 		playerRigidBody.AddRelativeForce(movement * movementSpeed);
-		Vector3 rotation = new Vector3(0f, moveHorizontal, 0f)* rotationSpeed;
-		// playerRigidBody.MoveRotation(Quaternion.Euler((transform.rotation.eulerAngles)* Time.deltaTime)* playerRigidBody.rotation);
-		playerRigidBody.AddRelativeTorque(rotation);
 	}
 }
