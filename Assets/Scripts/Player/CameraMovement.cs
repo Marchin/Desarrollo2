795491df74fe2a51d02822350701b11d44a37947
Marchin <mmonterrosa94@gmail.com>
2018-05-09 12:24:51 -0300

    Added Gun Raycast Script

--- Assets/Scripts/Player/CameraMovement.cs
+++ Assets/Scripts/Player/CameraMovement.cs
@@ -3,28 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
-	[SerializeField] float rotationSpeed;
-	[SerializeField] float min;
-	[SerializeField] float max;
+	[SerializeField] float cameraX;
+	[SerializeField] float cameraY;
+	[SerializeField] bool invertedCam = true;
+	[SerializeField] float MinimumX;
+	[SerializeField] float MaximumX;
+	Camera playerCamera;
 	Rigidbody playerRigidBody;
 
 	void Start() {
+		playerCamera = GetComponentInChildren<Camera>();
 		playerRigidBody = GetComponent<Rigidbody>();
 	}
 
 	void Update() {
-		transform.rotation = Quaternion.Euler(new Vector3(
-			Mathf.Clamp(transform.rotation.x, min, max),
-			transform.rotation.y,
-			transform.rotation.z)
-		);
+		float cameraVertical = Input.GetAxis("Mouse Y")* (invertedCam? - 1f : 1f);
+		Vector3 rotationY = new Vector3(cameraVertical, 0f, 0f)* cameraY;
+		playerCamera.transform.Rotate(rotationY);
+		playerCamera.transform.localRotation = ClampRotationAroundXAxis(playerCamera.transform.localRotation);
 	}
 
 	void FixedUpdate() {
 		float cameraHorizontal = Input.GetAxis("Mouse X");
-		float cameraVertical = Input.GetAxis("Mouse Y");
-		Vector3 rotation = new Vector3(cameraHorizontal, cameraVertical, 0f)* rotationSpeed;
-		playerRigidBody.AddRelativeTorque(rotation);
+		if (cameraHorizontal == 0f) {
+			playerRigidBody.angularVelocity = Vector3.zero;
+		} else {
+			Vector3 rotationX = new Vector3(0f, cameraHorizontal, 0f);
+			playerRigidBody.angularVelocity = rotationX * cameraX;
+		}
+	}
+
+	Quaternion ClampRotationAroundXAxis(Quaternion q) {
+		q.x /= q.w;
+		q.y /= q.w;
+		q.z /= q.w;
+		q.w = 1.0f;
+
+		float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+
+		angleX = Mathf.Clamp(angleX, MinimumX, MaximumX);
+
+		q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+		return q;
 	}
 
 }
