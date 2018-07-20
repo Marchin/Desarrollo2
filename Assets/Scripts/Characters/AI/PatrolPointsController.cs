462d596366022a2f30b0c9fcd1c6c639b37c13f8
Marchin <mmonterrosa94@gmail.com>
2018-07-20 18:22:06 -0300

    Update AI Patroling

--- Assets/Scripts/Characters/AI/PatrolPointsController.cs
+++ Assets/Scripts/Characters/AI/PatrolPointsController.cs
@@ -6,15 +6,22 @@ public class PatrolPointsController : MonoBehaviour {
     [SerializeField] PatrolPointsSet m_pps;
 
     public bool RequestPatrolPoint(ref int aiCurrentPoint, ref Vector3 newPosition, bool isEnemy) {
-        int point = Random.Range(0, m_pps.patrolPoints.Length - 1);
+        bool success = false;
+        int point = -1;
         List<int> occupied = (isEnemy? m_pps.occupiedEnemies : m_pps.occupiedAllies);
-        if (IsPointFree(occupied, point, aiCurrentPoint)) {
-            newPosition = m_pps.patrolPoints[point].position;
-            aiCurrentPoint = point;
-            return true;
-        } else {
-            return false;
+        for (int i = 0; i < m_pps.patrolPoints.Length; i++) {
+            if (point == -1 || (Vector3.Distance(newPosition, m_pps.patrolPoints[i].position) <
+                    (Vector3.Distance(newPosition, m_pps.patrolPoints[(point < 0 ? 0 : point)].position)))) {
+
+                if (IsPointFree(occupied, i, aiCurrentPoint)) {
+                    point = i;
+                    newPosition = m_pps.patrolPoints[point].position;
+                    aiCurrentPoint = point;
+                    success = true;
+                }
+            }
         }
+        return success;
     }
 
     bool IsPointFree(List<int> list, int point, int aiCurrentPoint) {
